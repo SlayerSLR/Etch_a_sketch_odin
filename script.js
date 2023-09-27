4e84cd2ebadf24a3fc621767e049421c77a9b129
SlayerSLR <aja15070@gmail.com>
2023-09-27 16:49:36 +0530

    change ui disable rightclick on canvas

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let i=0,gridId,boxSize=16,boxCount,flag=0;
 let container= document.querySelector(".gridContainer");
 let size= document.querySelector("#box");
 size.addEventListener("input", createGrid);
-value=document.querySelector("#value");
+value=document.querySelector(".value");
 
-value.textContent=boxSize;
+value.textContent=`${boxSize}x${boxSize}`;
 boxCount=boxSize*boxSize;
 makeBox();
 
 function createGrid(evt){
     container.innerHTML="";
     boxSize=evt.target.value;
-    value.textContent=boxSize;
+    value.textContent=`${boxSize}x${boxSize}`;
     console.log(`boxSize is ${boxSize}`);
     boxCount=boxSize*boxSize;
     console.log(`createGrid pass`)
@@ -33,12 +33,33 @@ function makeBox(){
         grid.setAttribute(`style`,`height:${x}px; width:${x}px`);
         container.appendChild(grid);
         //console.log(`div ${c} created`);
-        grid.addEventListener('mousedown',listener1);
-        grid.addEventListener('mouseover',colorAdd);
-        grid.addEventListener('mouseup',listener2);    
+
+        grid.addEventListener('mousedown',(e)=> {
+            if(e.button==0){
+                flag+=1;
+                colorAdd(e.target.id);
+                console.log(`listener1 executed flag=${flag}`);
+            }
+            else{e.preventDefault}
+        });
+
+        grid.addEventListener('mouseover',(e)=> {
+            colorAdd(e.target.id);
+        });
+
+        grid.addEventListener('mouseup',(e) => {
+            if(e.button == 0){
+                flag-=1;
+                colorAdd(e.target.id);
+                console.log(`listener2 executed flag=${flag}`);
+            }
+            else(e.preventDefault)
+        });
+           
         grid.addEventListener(`dragstart`,(event) => {
             event.preventDefault();
         });
+
         grid.addEventListener('drop',(event) => {
             event.prefventDefault();
         })
@@ -49,28 +70,28 @@ console.log("loop parsed");
 
 grid=document.querySelector(".etch");
 
-function listener1(e){
-    flag+=1;
+/*function listener1(e){
+    if(e.button== 0){flag+=1;}
     console.log(`listener1 executed flag=${flag}`);
     colorAdd(e);
-}
+} */
 
-function listener2(){
-    flag-=1;
+/*function listener2(){
+    if(e.button==0){flag-=1;}
     console.log(`listener2 executed flag=${flag}`);
-}
+}*/
 
 function colorAdd(e){
     let i,j,k;
     i=Math.floor(Math.random()*255);
     j=Math.floor(Math.random()*255);
     k=Math.floor(Math.random()*255);
-    grid=document.getElementById(e.target.id);
+    grid=document.getElementById(e);
     //grid.setAttribute('style',`background-color: rgb(${i},${j},${k})`);
     //grid.setAttribute('style',`background-color: rgb(0,0,0)`);
     if(flag==1){
     grid.style.backgroundColor = `rgb(${i},${j},${k})`;
-    console.log("color changed successfully");
+    console.log(`color of div ${e} changed to rgb(${i},${j},${k})`);
     }
     
 }
