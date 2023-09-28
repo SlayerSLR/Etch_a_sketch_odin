65993ebdf734eb07023f245c5533dbbe6c0e3640
SlayerSLR <aja15070@gmail.com>
2023-09-29 00:35:07 +0530

    fix save option,add clear canvas button

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function makeBox(){
         container.appendChild(grid);
         //console.log(`div ${c} created`);
 
-        grid.addEventListener('mousedown',(e)=> {
+         grid.addEventListener('mousedown',(e)=> {
             if(e.button==0){
                 flag+=1;
                 colorAdd(e.target.id);
@@ -76,7 +76,7 @@ function makeBox(){
 
         grid.addEventListener('drop',(event) => {
             event.prefventDefault();
-        })
+        }) 
 }
 }
 
@@ -91,17 +91,6 @@ console.log("loop parsed");
 
 grid=document.querySelector(".etch");
 
-/*function listener1(e){
-    if(e.button== 0){flag+=1;}
-    console.log(`listener1 executed flag=${flag}`);
-    colorAdd(e);
-} */
-
-/*function listener2(){
-    if(e.button==0){flag-=1;}
-    console.log(`listener2 executed flag=${flag}`);
-}*/
-
 function mono(){
     monochrome=true;
     flag=0;
@@ -112,17 +101,22 @@ function multicolor(){
     flag=0;
 }
 
-function shader(){
-    alert("This feature will be available soon");
+function clearCanvas(){
+    for(c=0;c<boxCount;c++){
+        grid=document.getElementById(c);
+        grid.style.backgroundColor=`beige`;
+    }
+    console.log("clear() run")
 }
+
+
 function colorAdd(e){
     let i,j,k;
     i=Math.floor(Math.random()*255);
     j=Math.floor(Math.random()*255);
     k=Math.floor(Math.random()*255);
     grid=document.getElementById(e);
-    //grid.setAttribute('style',`background-color: rgb(${i},${j},${k})`);
-    //grid.setAttribute('style',`background-color: rgb(0,0,0)`);
+    
     if(flag==1){
         if(monochrome){
             grid.style.backgroundColor = `rgb(0,0,0)`;   
